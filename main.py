fd714aacb2369a83553717eb78f6a1a99d385a18
tri10 <shubhi.blog@gmail.com>
2020-04-06 00:27:21 +0530

    CLI complete

--- main.py
+++ main.py
@@ -7,12 +7,27 @@ import re
 import os
 import time
 import gdownloader
+import argparse
+
+#  For calculating time 
+def timeme(fn):
+	def dfn(*args):
+		start = time.time()
+		r_value = fn(*args)
+		print('[#] Total time taken by {} : {:.2f}s'.format(fn.__name__,time.time()-start))
+		return r_value
+	return dfn
+
 class Unacdemy(object):
 	"""Simple Unacdemy Notes Downloading Utility"""
 
 	def __init__(self):
-		self.url = r'https://docs.google.com/spreadsheets/d/e/2PACX-1vQUwyeyCbReMBmABf-Q-XqG40oB5KrDQoUlLMpDZhBu18YasgWI72pAyH4beYolw95ylxQJdPqSWcig/pubhtml'
+		#  initializing instance variables
+		self.classes = [['s2','s21','s23','s30','s24 softmerge','s22 softmerge','s18','s19','s15','s19 softmerge','s10','s10 softmerge','s25'],['s7','s0','s17']]
+		self.pattern = re.compile(r'd\/(.*?)\/view')
+		self.url = '''https://docs.google.com/spreadsheets/d/e/2PACX-1vQUwyeyCbReMBmABf-Q-XqG40oB5KrDQoUlLMpDZhBu18YasgWI72pAyH4beYolw95ylxQJdPqSWcig/pubhtml'''
 	
+	#  Collecting data 
 	def run(self):
 		self.soup = self.getSoup(self.url)
 		#  Checking for cache 
@@ -21,7 +36,7 @@ class Unacdemy(object):
 				self.db = json.load(f)
 		else:
 			#  requesting and storing in json file
-			self.fetch_links(self.url)
+			self.fetch_links(self.getSoup(self.url),self.classes)
 			self.tojson(self.db)
 
 	
@@ -31,10 +46,8 @@ class Unacdemy(object):
 		return BeautifulSoup(self.r.content,'html.parser')
 	
 	#  Scraping links with Chapter names	
-	def fetch_links(self,soup):
+	def fetch_links(self,soup,classes):
 		''' Messy code / Worst way to do this'''
-		#  physics , chemistry , maths , chapter names | class of tags for note links
-		classes = [['s2','s21','s23','s30','s24 softmerge','s22 softmerge','s18','s19','s15','s19 softmerge','s10','s10 softmerge','s25'],['s7','s0','s17']]
 		self.db = {'physics':{},'chemistry':{},'maths':{}}
 		for elements in soup.find_all('tr'):
 			temp_ch = elements.find('td',class_=classes[0])
@@ -56,9 +69,10 @@ class Unacdemy(object):
 					self.db[subject][chapter].append(temp_link.find('a').get('href'))
 			except :
 				pass
-		print([{(key,len(value.keys())):reduce(lambda acc,nk:acc+len(self.db[key][nk]),value.keys(),0)} for key,value in self.db.items()])
+		#  list comprehension for printing total Chapters/Notes for different subject
+		# print([{(key,len(value.keys())):reduce(lambda acc,nk:acc+len(self.db[key][nk]),value.keys(),0)} for key,value in self.db.items()])
 
-	#  Cacheing	
+	#  Cacheing
 	def  tojson(self,db):
 		with open('db.json','w') as f:
 			json.dump(db,f,ensure_ascii=False,indent=4)
@@ -66,35 +80,60 @@ class Unacdemy(object):
 	#  to unshorten the url
 	def _unshorten(self,url):
 		soup = self.getSoup(url)
-		ulink = soup.find('a').get_text()
-		return requests.head(ulink, allow_redirects=True).url
+		ulink = soup.find('a').get_text() #  scraping bitly url
+		return requests.head(ulink, allow_redirects=True).url #  returns shared link of file in google drive 
 	
 	#  Fetching id of gdrive link
 	def fetch_id(self,url):
-		pattern = re.compile(r'd\/(.*?)\/view')
-		return pattern.findall(self._unshorten(url))[0]
+		return self.pattern.findall(self._unshorten(url))[0]
 
 	#  Downloading content using id
-	def download_content(self,subject,chapter,path=os.path.dirname(os.path.abspath(__file__))):
+	@timeme
+	def download_content(self,subject,chapter,path,verbose=False):
 		apath = os.path.join(path,chapter)
 		if not os.path.isdir(apath):
 			os.mkdir(apath)
+		else:
+			pass
+			print('[!] Folder already exist')
+			user = input('continue ? (y/n)..')
+			if user == 'y':
+				apath = os.path.join(path,input('enter new name for folder :'))
+				os.mkdir(apath)
+			else:
+				exit(0)
 		for index,links in enumerate(self.db[subject][chapter]):
 			ids = self.fetch_id(links)
 			f_path = os.path.join(apath,f'{chapter} L{index+1}.pdf')
-			print('Downloading Lecture-',index+1)
+			if verbose:
+				print('[.] Downloading Lecture-',index+1)
 			gdownloader.downloadfile(ids,f_path)
-			print('Notes Saved to ',f_path)
-	def timeme(fn):
-		def dfn(*args):
-			start = time.time()
-			r_value = fn(*args)
-			print('Total time taken :',time.time()-start)
-			return r_value
-		return dfn
-
+			if verbose:
+				print('[+]Notes Saved to ',f_path)
+		print('Downloaded {} Notes in {}!'.format(chapter,apath))
 
 if __name__ == '__main__':
+	parser = argparse.ArgumentParser(description='Unacdemy JEE Notes Downloader Utility coded by Tri10',usage='python3 main.py -s SUBJECT -c "CHAPTER"')
+	group = parser.add_mutually_exclusive_group()
+	parser.add_argument('-s','--subject',required=True,help='Subject of Notes (case sensitive) eg.maths',type=str,choices=['physics','chemistry','maths'])
+	parser.add_argument('-c','--chapter',type=str,required=True,help='Chapter Name (case sensitive) eg.Application of derivative')
+	parser.add_argument('-d','--dir',type=str,help='Specify directory to store notes',default=os.path.dirname(os.path.abspath(__file__)))
+	group.add_argument('-v','--verbose',action="store_true",default=False)
+	args = parser.parse_args()
 	obj = Unacdemy()
 	obj.run()
-	obj.download_content('maths',"Application of Derivatives")
+	if args.chapter in obj.db[args.subject].keys():
+		print('> Started Downloading Notes...')
+		obj.download_content(args.subject,args.chapter,args.dir,args.verbose)
+	else:
+		print('~Chapter not found , maybe you mispelled ')
+		for chapters in obj.db[args.subject].keys():
+			if chapters.lower().count(args.chapter):
+				print('[?] Do yo mean "{}" '.format(chapters))
+				break
+		else:
+			print('[*] try finding chapter name in db.json file')
+
+
+	
+	
